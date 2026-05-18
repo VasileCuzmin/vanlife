316e9110bde79f3cc3c59849bebeacbb858114aa
vasile.cuzmin <cuzmin@hlsr.com>
2026-05-18 10:36:31 +0300

    get data from firestore/firebase

--- api.js
+++ api.js
@@ -1,44 +1,115 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    doc,
+    where
+} from "firebase/firestore/lite";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCQr-uVr9abjHA8qL-4KmzHDDiIwqOT89M",
+    authDomain: "vanlife-2664a.firebaseapp.com",
+    projectId: "vanlife-2664a",
+    storageBucket: "vanlife-2664a.firebasestorage.app",
+    messagingSenderId: "710615001192",
+    appId: "1:710615001192:web:12db3b1fd87dd8946633dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+//Refactoring the fetch functions below
+
+const vansCollectionsRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionsRef);
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }));
+
+    return vans;
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id);
+    const snapshot = await getDoc(docRef);
+    return {
+        ...snapshot.data,
+        id: snapshot.id
     }
-    const data = await res.json()
-    return data.vans
 }
 
-export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
+export async function getHostVans() {
+    const q = query(vansCollectionsRef, where("hostId", "==", "123"));
+    const snapshot = await getDocs(q);
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }));
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-
-    return data
+    return vans;
 }
+
+export async function getHostVan(id) {
+    const q = query(vansCollectionsRef, where("hostId", "==", "123"));
+    const snapshot = await getDocs(q);
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }));
+
+    return vans;
+}
+
+
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+
+// export async function loginUser(creds) {
+//     const res = await fetch("/api/login",
+//         { method: "post", body: JSON.stringify(creds) }
+//     )
+//     const data = await res.json()
+
+//     if (!res.ok) {
+//         throw {
+//             message: data.message,
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+
+//     return data
+// }
